3004d64bf82ee0b4accefb8e91be2979e45db2ea
Daniil <danlee65071@gmail.com>
2022-11-11 16:05:39 +0300

    init Scene

---
+++ includes/Scene.hpp
@@ -0,0 +1,25 @@
+/*
+ * @ Author: Daniil Lee
+ * @ Create Time: 2022-11-10 13:56:09
+ * @ Modified by: Daniil Lee
+ * @ Modified time: 2022-11-11 16:05:03
+ * @ Description: Scene class implemetation
+ */
+
+#pragma once
+
+/*
+ *   Class Scene.
+ *   Class Scene has singleton realization.
+ *   In Scene class objects are create
+ */
+class Scene
+{
+private:
+    Scene(const Scene &scene);
+    Scene &operator=(const Scene &scene);
+
+public:
+    Scene();
+    ~Scene();
+};
